8535dba9d0ee2258376c1ba4b85495450689c0d4
goosefin <agustinafin96@gmail.com>
2022-08-20 12:09:44 -0400

    Created book schema

---
+++ models/books.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+// Schema
+const bookSchema = new mongoose.Schema({
+    title: {type:String, required:true},
+    author: {type:String, required:true},
+    starRating: {type:Number, min:0, max:5, default:0},
+    review: String,
+    status: Boolean,
+    ownedBy: String
+})
+
+const Book = mongoose.model('Book', bookSchema)
+module.exports = Book
