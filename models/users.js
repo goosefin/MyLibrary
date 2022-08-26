03b2830f60e88d1fa7b641bd977648c7fa6eae24
goosefin <agustinafin96@gmail.com>
2022-08-26 18:04:15 -0400

    fixed show page styling

--- models/users.js
+++ models/users.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose')
+//const book = require('./books')
 
 const userSchema = new mongoose.Schema({
     username: {type: String, required:true, unique:true},
     email: {type:String, required:true, unique:true},
     password: {type:String, required:true},
+    //books: [Book]
+    //book: new.mongoose.Schema({
+        // title: {type:String, required:true},
+        // author: {type:String, required:true},
+        // starRating: {type:String, min:0, max:5, default:0},
+        // review: String,
+        // status: Boolean,
+        // isbn: String,
+        // borrowed: Boolean,
+        // borrowedBy: String,
+        // })
 })
 
 const User = mongoose.model('User',userSchema)
--- public/js/app.js
+++
@@ -1,9 +0,0 @@
-const isBorrowed = () =>{
-    let borrowedInput = document.getElementsByClassName('borrowed')
-    borrowedInput.classList.remove('borrowed')
-    console.log('clicked')
-}
-
-const mustLogIn = () =>{
-    alert('You must be logged in to do that')
-}
